6fb6a146844cb5ae4c66cab62cd19509b53e6f98
Gabriel T <gabriel.tapuc@gmail.com>
2019-09-07 19:45:00 -0400

    Final push.

--- src/components/Seo.js
+++ src/components/Seo.js
@@ -3,6 +3,7 @@ import { Helmet } from "react-helmet";
 
 const title = 'Gabriel Tapuc | Web and Mobile App Freelancer';
 const description = 'Build quality web and mobile applications, stress-free.'
+const siteName = 'https://gabrieltapuc.com/';
 
 // Todo images
 const Seo = () => (
@@ -12,11 +13,11 @@ const Seo = () => (
 
     <meta property="og:title" content={title} />
     <meta property="og:description" content={description} />
-    {/*<meta property="og:image" content={siteName + img} />*/}
+    <meta property="og:image" content={siteName + 'ogImage.jpg'} />
 
     <meta name="twitter:description" content={description} />
-    {/*<meta name="twitter:image" content={siteName + img} />*/}
-    {/*<meta name="twitter:image:alt" content={description} />*/}
+    <meta name="twitter:image" content={siteName + 'ogImage.jpg'} />
+    <meta name="twitter:image:alt" content={description} />
     <meta name="twitter:title" content={title} />
   </Helmet>
 );
