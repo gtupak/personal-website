642d21f89af440d132e88e0d768137736a1f9654
Gabriel T <gabriel.tapuc@gmail.com>
2019-09-10 16:35:28 -0400

    Added google ads conversion tracking.

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,15 +34,32 @@ module.exports = {
         icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
       }
     },
+    // {
+    //   resolve: `gatsby-plugin-google-analytics`,
+    //   options: {
+    //     trackingId: "UA-66133549-3",
+    //     head: true,
+    //     respectDNT: true, // do not track
+    //     // Delays sending pageview hits on route update (in milliseconds)
+    //     pageTransitionDelay: 0,
+    //   }
+    // }
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: `gatsby-plugin-google-gtag`,
       options: {
-        trackingId: "UA-66133549-3",
-        head: true,
-        respectDNT: true, // do not track
-        // Delays sending pageview hits on route update (in milliseconds)
-        pageTransitionDelay: 0,
-      }
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "UA-66133549-3", // Google Analytics
+          "AW-800491324", // Google Ads / Adwords / AW
+        ],
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+          // Setting this parameter is also optional
+          respectDNT: true
+        },
+      },
     }
   ],
   siteMetadata: {
